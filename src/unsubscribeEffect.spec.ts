8667ccd02def1b8a17a02f5bb8d72db00aaa35a0
Wolfger Schramm <wolfger@spearwolf.de>
2021-09-18 13:30:17 +0200

    add more tests

---
+++ src/unsubscribeEffect.spec.ts
@@ -0,0 +1,87 @@
+import {createEffect} from './createEffect';
+import {createSignal} from './createSignal';
+
+describe('unsubscribe as return function from effect callback', () => {
+  it('should be called before recalling the effect callback', () => {
+    const [a, setA] = createSignal(123);
+    const [b, setB] = createSignal('abc');
+
+    const valA = jest.fn();
+    const valB = jest.fn();
+
+    const unsubscribeA = jest.fn();
+    const unsubscribeB = jest.fn();
+
+    let effectCallCount0 = 0;
+    let effectCallCount1 = 0;
+    const subscriptionOrder: any[] = [];
+
+    const clearAllMocks = () => {
+      jest.clearAllMocks();
+      effectCallCount0 = 0;
+      effectCallCount1 = 0;
+      subscriptionOrder.length = 0;
+    };
+
+    createEffect(() => {
+      ++effectCallCount0;
+      valA(a());
+
+      createEffect(() => {
+        ++effectCallCount1;
+        valB(b());
+
+        const _b = b();
+        return () => {
+          subscriptionOrder.push(_b);
+          unsubscribeB(_b);
+        };
+      });
+
+      const _a = a();
+      return () => {
+        subscriptionOrder.push(_a);
+        unsubscribeA(_a);
+      };
+    });
+
+    expect(effectCallCount0).toBe(1);
+    expect(effectCallCount1).toBe(1);
+
+    expect(valA).toBeCalledWith(123);
+    expect(valB).toBeCalledWith('abc');
+
+    expect(unsubscribeA).toBeCalledTimes(0);
+    expect(unsubscribeB).toBeCalledTimes(0);
+
+    clearAllMocks();
+
+    setB('foo');
+
+    expect(effectCallCount0).toBe(0);
+    expect(effectCallCount1).toBe(1);
+
+    expect(valA).toBeCalledTimes(0);
+    expect(valB).toBeCalledWith('foo');
+
+    expect(unsubscribeA).toBeCalledTimes(0);
+    expect(unsubscribeB).toBeCalledWith('abc');
+
+    expect(subscriptionOrder).toEqual(['abc']);
+
+    clearAllMocks();
+
+    setA(43);
+
+    expect(effectCallCount0).toBe(1);
+    expect(effectCallCount1).toBe(1);
+
+    expect(valA).toBeCalledWith(43);
+    expect(valB).toBeCalledWith('foo');
+
+    expect(unsubscribeA).toBeCalledWith(123);
+    expect(unsubscribeB).toBeCalledWith('foo');
+
+    expect(subscriptionOrder).toEqual(['foo', 123]);
+  });
+});
