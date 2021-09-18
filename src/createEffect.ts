b6ea8fced2823afe2142e8e071e3a3b2e0afb2b3
Wolfger Schramm <wolfger@spearwolf.de>
2021-09-18 12:11:56 +0200

    add optional unsubscribe return callbacks to effects

--- src/createEffect.ts
+++ src/createEffect.ts
@@ -1,13 +1,13 @@
 import {Effect} from './Effect';
-import {getCurrentEffect, runWithinEffect} from './globalEffectStack';
-import {EffectCallback} from './types';
+import {getCurrentEffect} from './globalEffectStack';
+import {EffectCallback, VoidCallback} from './types';
 
-export function createEffect(callback: EffectCallback): () => void {
+export function createEffect(callback: EffectCallback): VoidCallback {
   const effect = new Effect(callback);
 
   getCurrentEffect()?.addChild(effect);
 
-  runWithinEffect(effect, callback);
+  effect.run();
 
   return () => {
     effect.unsubscribe();
