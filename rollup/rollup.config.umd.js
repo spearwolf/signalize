5a1d95ec00c0185d4f55bd80f7c03d4a510f2649
Wolfger Schramm <wolfger@spearwolf.de>
2021-09-18 11:29:06 +0200

    include externals deps into umd build output

--- rollup/rollup.config.umd.js
+++ rollup/rollup.config.umd.js
@@ -5,6 +5,7 @@ import rollupConfigShared from './rollup.config.shared';
 
 export default rollupConfigShared(
   'es2017',
+  false, // <- include externals in build output
   ({outputDir, packageJson: {rollupBuildName: name}}) => ({
     output: {
       name,
