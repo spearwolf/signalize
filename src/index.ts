b052ce916676e9d55330c5111a9ef1fbb5b2920f
Wolfger Schramm <wolfger@spearwolf.de>
2021-09-19 13:09:32 +0200

    update README

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {batch} from './batch';
 export {createEffect} from './createEffect';
 export {createMemo} from './createMemo';
-export {createSignal, isSignal, touch, value} from './createSignal';
+export {createSignal, touch, value} from './createSignal';
