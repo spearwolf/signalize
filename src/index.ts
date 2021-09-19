d9d072d18b5dca2c42b5acfb939470edb5fbcebf
Wolfger Schramm <wolfger@spearwolf.de>
2021-09-19 12:24:28 +0200

    add createMemo helper

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {batch} from './batch';
 export {createEffect} from './createEffect';
+export {createMemo} from './createMemo';
 export {createSignal, isSignal, touch, value} from './createSignal';
