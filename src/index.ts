3cea8056b10f7fe2ebd93b8ec796e061bb3b7143
Wolfger Schramm <wolfger@spearwolf.de>
2023-02-21 09:26:47 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {batch} from './batch';
 export {createEffect, onCreateEffect} from './createEffect';
 export {createMemo} from './createMemo';
 export {createSignal, isSignal, touch, value} from './createSignal';
+export * from './types';
