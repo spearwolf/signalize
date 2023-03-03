16d88a22bc3913cc854d3782eb7005f82de77010
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-03 09:49:59 +0100

    add jest error message extension

--- jest.config.js
+++ jest.config.js
@@ -126,6 +126,7 @@ export default {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["jest-expect-message"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
