bb952d7867a39a55c5726ea87b6680028f18833e
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-06 13:38:49 +0100

    remove redundant Effect.runAgain

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 export const $signal = Symbol('signal');
-export const $runAgain = Symbol('runAgain');
 export const $destroySignal = Symbol('destroySignal');
 export const $createEffect = Symbol('createEffect');
 export const $destroyEffect = Symbol('destroyEffect');
