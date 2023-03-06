e00238402f440dfb98f4ac945883119dedcad38a
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-06 09:37:39 +0100

    WIP refactor effects subscriptions logic

--- src/assert-helpers.ts
+++ src/assert-helpers.ts
@@ -4,10 +4,15 @@ import {getEffectsCount} from './createEffect';
 
 import {globalDestroySignalQueue, globalEffectQueue} from './globalQueues';
 
-export function assertEffectsCount(count: number) {
+const namespacePrefix = (namespace?: string) =>
+  namespace ? `${namespace}: ` : '';
+
+export function assertEffectsCount(count: number, namespace?: string) {
   expect(
     getEffectsCount(),
-    `Number of active effects should be ${count} but is ${getEffectsCount()}`,
+    `${namespacePrefix(
+      namespace,
+    )}Number of active effects should be ${count} but is ${getEffectsCount()}`,
   ).toBe(count);
 }
 
@@ -22,21 +27,31 @@ export const saveEffectSubscriptionsCount = (initial?: boolean) => {
   return g_effectCount;
 };
 
-export function assertEffectSubscriptionsCountChange(deltaCount: number) {
+export function assertEffectSubscriptionsCountChange(
+  deltaCount: number,
+  namespace?: string,
+) {
   const beforeCount = g_effectCount - g_initialEffectCount;
   const count = saveEffectSubscriptionsCount() - g_initialEffectCount;
   expect(
     count,
-    `Effect subscriptions count change delta should be ${deltaCount} but is ${
+    `${namespacePrefix(
+      namespace,
+    )}Effect subscriptions count change delta should be ${deltaCount} but is ${
       count - beforeCount
     }`,
   ).toBe(beforeCount + g_initialEffectCount + deltaCount);
 }
 
-export function assertEffectSubscriptionsCount(count: number) {
+export function assertEffectSubscriptionsCount(
+  count: number,
+  namespace?: string,
+) {
   expect(
     count,
-    `Effect subscriptions count should be ${count} but is ${
+    `${namespacePrefix(
+      namespace,
+    )}Effect subscriptions count should be ${count} but is ${
       getSubscriptionCount(globalEffectQueue) - g_initialEffectCount
     }`,
   ).toBe(getSubscriptionCount(globalEffectQueue) - g_initialEffectCount);
@@ -58,20 +73,33 @@ export const saveSignalDestroySubscriptionsCount = (initial?: boolean) => {
 
 export function assertSignalDestroySubscriptionsCountChange(
   deltaCount: number,
+  namespace?: string,
 ) {
   const beforeCount =
     g_signalDestroySubscriptionsCount -
     g_initialSignalDestroySubscriptionsCount;
-  const count = saveSignalDestroySubscriptionsCount();
-  expect(beforeCount + deltaCount).toBe(
-    count - g_initialSignalDestroySubscriptionsCount,
-  );
+  const current =
+    saveSignalDestroySubscriptionsCount() -
+    g_initialSignalDestroySubscriptionsCount;
+  expect(
+    beforeCount + deltaCount,
+    `${namespacePrefix(
+      namespace,
+    )}: Signal destroy subscriptions count should be ${
+      beforeCount + deltaCount
+    } but is ${current}`,
+  ).toBe(current);
 }
 
-export function assertSignalDestroySubscriptionsCount(count: number) {
+export function assertSignalDestroySubscriptionsCount(
+  count: number,
+  namespace?: string,
+) {
   expect(
     count,
-    `Signal destroy subscriptions count should be ${count} but is ${
+    `${namespacePrefix(
+      namespace,
+    )}Signal destroy subscriptions count should be ${count} but is ${
       getSubscriptionCount(globalDestroySignalQueue) -
       g_initialSignalDestroySubscriptionsCount
     }`,
