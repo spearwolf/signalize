9338e294c60b4093ce929cf66e2f1d841c4bb7f1
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-07 10:04:00 +0100

    improve batch

--- src/batch.ts
+++ src/batch.ts
@@ -1,21 +1,12 @@
 import {Effect} from './Effect';
 import {globalEffectQueue} from './global-queues';
 import {BatchCallback} from './types';
-import {UniqIdGen} from './UniqIdGen';
 
 class Batch {
   static current?: Batch;
 
-  private static readonly idCreator = new UniqIdGen('ba');
-
-  readonly id: symbol;
-
   readonly #effects = new Map<symbol, Effect>();
   readonly #delayedEffects = new Set<symbol>();
-
-  constructor() {
-    this.id = Batch.idCreator.make();
-  }
 
   batch(effect: Effect) {
     this.#effects.set(effect.id, effect);
@@ -23,23 +14,23 @@ class Batch {
   }
 
   execute() {
-    const runEffects: symbol[] = [];
+    const runEffectIds: symbol[] = [];
 
     // TODO write tests for this
     for (const effectId of this.#delayedEffects) {
       const effect = this.#effects.get(effectId);
       if (
-        effect.parentEffect == null ||
+        effect?.parentEffect == null ||
         !this.#delayedEffects.has(effect.parentEffect.id)
       ) {
-        runEffects.push(effectId);
+        runEffectIds.push(effectId);
       }
     }
 
     this.#effects.clear();
     this.#delayedEffects.clear();
 
-    globalEffectQueue.emit(runEffects);
+    globalEffectQueue.emit(runEffectIds);
   }
 }
 
