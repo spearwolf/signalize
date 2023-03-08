372c3ec4b26d9ce307a2b1cbbb73673f1a26815c
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-08 11:45:59 +0100

    add @memo class method decorator

--- src/createMemo.ts
+++ src/createMemo.ts
@@ -3,6 +3,8 @@ import {createEffect} from './effects-api';
 import {createSignal, getSignal} from './createSignal';
 import {globalDestroySignalQueue} from './global-queues';
 
+// TODO memo -> autorun: false by default ?
+
 export function createMemo<Type>(callback: () => Type): SignalReader<Type> {
   const [signal, set] = createSignal<Type>();
   const [, unsubscribe] = createEffect(() => set(callback()));
