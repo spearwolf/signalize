77674b160818fd39083b000bcbae8f46e92f9f84
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-10 08:33:05 +0100

    use @effect({autorun?}) syntax, remove @asyncEffect for now

--- src/decorators.effect.spec.ts
+++ src/decorators.effect.spec.ts
@@ -15,7 +15,7 @@ describe('@effect is a class method decorator', () => {
       plahValue = 0;
       plahCallCount = 0;
 
-      @effect plah() {
+      @effect() plah() {
         this.plahValue = this.foo * 100 + this.foo + this.bar;
         ++this.plahCallCount;
       }
@@ -62,6 +62,55 @@ describe('@effect is a class method decorator', () => {
     expect(foo.foo).toBe(4);
     expect(foo.plahValue).toBe(333);
     expect(foo.plahCallCount).toBe(4);
+
+    destroySignalsAndEffects(foo);
+  });
+
+  it('autorun: false', () => {
+    class Foo {
+      @signal() accessor foo = 1;
+      @signal() accessor bar = 10;
+
+      plahValue = 0;
+      plahCallCount = 0;
+
+      @effect({autorun: false}) plah() {
+        this.plahValue = this.foo * 100 + this.foo + this.bar;
+        ++this.plahCallCount;
+      }
+    }
+
+    const foo = new Foo();
+
+    expect(foo.foo).toBe(1);
+    expect(foo.bar).toBe(10);
+    expect(foo.plahValue).toBe(0);
+    expect(foo.plahCallCount).toBe(0);
+
+    foo.plah();
+
+    expect(foo.plahValue).toBe(111);
+    expect(foo.plahCallCount).toBe(1);
+
+    foo.foo = 2;
+    foo.bar = 20;
+
+    expect(foo.foo).toBe(2);
+    expect(foo.bar).toBe(20);
+    expect(foo.plahValue).toBe(111);
+    expect(foo.plahCallCount).toBe(1);
+
+    foo.plah();
+
+    expect(foo.plahValue).toBe(222);
+    expect(foo.plahCallCount).toBe(2);
+
+    foo.foo = 2;
+    foo.bar = 20;
+    foo.plah();
+
+    expect(foo.plahValue).toBe(222);
+    expect(foo.plahCallCount).toBe(2);
 
     destroySignalsAndEffects(foo);
   });
