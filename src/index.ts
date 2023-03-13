ff92b3b246388478bd30c1931fc7cd519dd14d6a
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-13 11:09:13 +0100

    add more TODOs

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,13 @@ export * from './effects-api';
 export * from './object-signals-and-effects';
 
 // TODO connect(), unconnect() signals !
-// TODO update README (missing decorators docs, rewrite introduction, add ts@5 / decorators stage-3 notes ...)
+
+// TODO docs: update README (missing decorators docs, rewrite introduction, add ts@5 / decorators stage-3 notes ...)
+
+// TODO docs: create awesome hero picture for this library
+
+// TODO docs: rename the library to 'signaler-effectus' ?
+
+// TODO docs: create a cheat sheet
+
+// TODO docs: add contribution guide (setup dev env, run tests, etc.)
