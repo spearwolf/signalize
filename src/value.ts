9c30c9ac80d49a756e4c67b4db9b0fa2cc139692
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-22 12:34:43 +0100

    improve types and value()

--- src/value.ts
+++ src/value.ts
@@ -1,8 +1,17 @@
 import {SignalReader} from './types';
 import {getSignal} from './createSignal';
+import {queryObjectSignal} from './object-signals-and-effects';
 
 // TODO value([obj, propKey])
 
+// export const value = <Type = unknown>(
+//   signalReader: SignalReader<Type>,
+// ): Type | undefined => getSignal(signalReader)?.value;
+
 export const value = <Type = unknown>(
-  signalReader: SignalReader<Type>,
-): Type | undefined => getSignal(signalReader)?.value;
+  ...args: [SignalReader<Type>] | [object, string | symbol]
+): Type | undefined =>
+  args.length === 1
+    ? getSignal(...args)?.value
+    : getSignal(queryObjectSignal(...args) as SignalReader<any> | undefined)
+        ?.value;
