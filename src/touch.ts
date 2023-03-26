3bb1265097868fdc7a503ea8df815858ef1642a3
Wolfger Schramm <wolfger@spearwolf.de>
2023-03-26 15:58:03 +0200

    touch: support object signal syntax

---
+++ src/touch.ts
@@ -0,0 +1,16 @@
+import {getSignalInstance, isSignal, writeSignal} from './createSignal';
+import {queryObjectSignal} from './object-signals-and-effects';
+import {SignalReader} from './types';
+
+function touch<Type>(source: SignalReader<Type>): void;
+function touch<O, K extends keyof O>(source: [O, K]): void;
+function touch(source: any) {
+  const signal = getSignalInstance(
+    isSignal(source) ? source : queryObjectSignal(...(source as [any, any])),
+  );
+  if (signal != null && !signal.muted && !signal.destroyed) {
+    writeSignal(signal.id, signal.value, {touch: true});
+  }
+}
+
+export {touch};
