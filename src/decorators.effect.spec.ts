125b16930347b5247eb7dea8598d9933268a4770
Wolfger Schramm <wolfger@spearwolf.de>
2023-11-22 13:50:09 +0100

    fix effect decorator types

--- src/decorators.effect.spec.ts
+++ src/decorators.effect.spec.ts
@@ -286,4 +286,53 @@ describe('@effect is a class method decorator', () => {
     destroySignalsAndEffects(foo);
     destroySignalsAndEffects(bar);
   });
+
+  it('the cleanup callback is called as expected', () => {
+    const cleanup = jest.fn();
+
+    class Foo {
+      @signal() accessor foo = 'foo';
+      @signal() accessor bar = 'bar';
+
+      plahValue: string = '';
+      plahCallCount = 0;
+
+      @effect({deps: ['foo', 'bar']}) plah() {
+        const val = `${this.foo}${this.bar}`;
+        this.plahValue = val;
+        ++this.plahCallCount;
+        return () => {
+          cleanup(val);
+        };
+      }
+
+      constructor() {
+        this.plah();
+      }
+    }
+
+    const foo = new Foo();
+
+    expect(foo.foo).toBe('foo');
+    expect(foo.bar).toBe('bar');
+    expect(foo.plahValue).toBe('foobar');
+    expect(foo.plahCallCount).toBe(1);
+    expect(cleanup).not.toHaveBeenCalled();
+
+    foo.foo = 'phoo';
+
+    expect(foo.foo).toBe('phoo');
+    expect(foo.plahValue).toBe('phoobar');
+    expect(foo.plahCallCount).toBe(2);
+    expect(cleanup).toHaveBeenCalledWith('foobar');
+
+    foo.bar = 'plah';
+
+    expect(foo.bar).toBe('plah');
+    expect(foo.plahValue).toBe('phooplah');
+    expect(foo.plahCallCount).toBe(3);
+    expect(cleanup).toHaveBeenCalledWith('phoobar');
+
+    destroySignalsAndEffects(foo);
+  });
 });
