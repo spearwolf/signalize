fa0524b14eea10e401991aabe5a416e4bcd10a71
Wolfger Schramm <wolfger@spearwolf.de>
2024-04-09 15:09:56 +0200

    add new lazy test

--- src/createSignal.lazy.spec.ts
+++ src/createSignal.lazy.spec.ts
@@ -7,18 +7,36 @@ describe('create lazy signal', () => {
 
     expect(val()).toBe('foo');
     expect(val()).toBe('foo');
-    expect(lazyFn).toBeCalledTimes(1);
+    expect(lazyFn).toHaveBeenCalledTimes(1);
 
     const lazyFn2 = jest.fn(() => 'bar');
     setValue(lazyFn2, {lazy: true});
 
     expect(val()).toBe('bar');
 
-    expect(lazyFn).toBeCalledTimes(1);
-    expect(lazyFn2).toBeCalledTimes(1);
+    expect(lazyFn).toHaveBeenCalledTimes(1);
+    expect(lazyFn2).toHaveBeenCalledTimes(1);
 
     setValue('plah');
 
     expect(val()).toBe('plah');
+  });
+
+  it('laziness is NOT catching on', () => {
+    const lazy0 = jest.fn(() => 'foo');
+    const lazy1 = jest.fn(() => 'bar');
+
+    const [val, setValue] = createSignal(lazy0, {lazy: true});
+
+    expect(val()).toBe('foo');
+    expect(val()).toBe('foo');
+    expect(lazy0).toHaveBeenCalledTimes(1);
+
+    setValue(lazy1);
+
+    expect(val()).toBe(lazy1);
+
+    expect(lazy0).toHaveBeenCalledTimes(1);
+    expect(lazy1).toHaveBeenCalledTimes(0);
   });
 });
