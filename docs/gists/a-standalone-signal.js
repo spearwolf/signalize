b724d7ab52c1f9d1224c7c4164b76eebde9d4b67
Wolfger Schramm <wolfger@spearwolf.de>
2025-08-21 10:05:53 +0200

    v0.21.1 remove docs/ folder and improve documentation

--- docs/gists/a-standalone-signal.js
+++
@@ -1,8 +0,0 @@
-import {createSignal} from '@spearwolf/signalize';
-
-const foo = createSignal(123);
-
-foo.get(); // => 123
-
-foo.set(456);
-foo.value; // => 456
--- docs/gists/signal-batch-func.js
+++
@@ -1,9 +0,0 @@
-import {batch} from '@spearwolf/signalize';
-
-// ...
-
-batch(() => {
-  setA(4);
-  setB(3);
-});
-// => sum of 4 and 3 is 7
